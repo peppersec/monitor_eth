6ef884684f4ac8733dfb03adb9bb29764b95a1bf
poma <Semenov.Roman@mail.ru>
2019-11-13 10:39:45 +0300

    docker-compose, healthcheck, loop in js
    (cherry picked from commit c7e1a15ae116576724017f8dc9793686a7bab3f2)

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
+const fetch = require('node-fetch')
 const Web3 = require('web3')
-require('dotenv').config()
 const web3 = new Web3('https://cloudflare-eth.com')
 const { toBN, toWei, fromWei } = require('web3-utils')
-const fetch = require('node-fetch')
-const { TOKEN, ADDRESS, THRESHOLD, CHAT_ID }  = process.env
+require('dotenv').config()
+const { ADDRESS, THRESHOLD, INTERVAL, TELEGRAM_NOTIFIER_BOT_TOKEN, TELEGRAM_NOTIFIER_CHAT_ID } = process.env
 
 async function main() {
-    let balance = await web3.eth.getBalance(ADDRESS)
-    balance = toBN(balance)
-    if( balance.lt(toBN(toWei(THRESHOLD))) ) {
-        const response = await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                chat_id: CHAT_ID,
-                text: `https://etherscan.io/address/${ADDRESS} balance is below ${THRESHOLD} ETH. \nit's ${fromWei(balance.toString(10))}`
-            }) // body data type must match "Content-Type" header
-        });
-        console.log('resp', response)
-    } else {
-        console.log('everythig is ok', `${ADDRESS} balance is ${fromWei(balance.toString(10))}`)
+    try {
+        let balance = await web3.eth.getBalance(ADDRESS)
+        balance = toBN(balance)
+        if( balance.lt(toBN(toWei(THRESHOLD))) ) {
+            const response = await fetch(`https://api.telegram.org/bot${TELEGRAM_NOTIFIER_BOT_TOKEN}/sendMessage`, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    chat_id: TELEGRAM_NOTIFIER_CHAT_ID,
+                    text: `https://etherscan.io/address/${ADDRESS} balance is below ${THRESHOLD} ETH. \nit's ${fromWei(balance.toString(10))}`
+                }) // body data type must match 'Content-Type' header
+            })
+            console.log('resp', response)
+        } else {
+            console.log('everythig is ok', `${ADDRESS} balance is ${fromWei(balance.toString(10))}`)
+        }
+    } catch (e) {
+        console.log(e)
+    } finally {
+        setTimeout(main, INTERVAL * 1000)
     }
 }
 
-main()
+if (process.argv.includes('healthcheck')) {
+    fetch(`https://api.telegram.org/bot${TELEGRAM_NOTIFIER_BOT_TOKEN}/getMe`)
+        .then(() => process.exit(0))
+        .catch(() => process.exit(101)) 
+} else {
+    main()
+}
